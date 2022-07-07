1f4fc7f76cef52926dc4e55c02c2c4b991977195
Pravesh22 <budhathokipravesh5@gmail.com>
2022-07-08 00:12:38 +0545

    delete method added

--- hash_map/basic_hash_table.py
+++ hash_map/basic_hash_table.py
@@ -6,6 +6,9 @@
 """This code gives basic overview of how dictionary works in python. 
 Dictionary in python acts as Hash_Table"""
 
+"""Note: This is basic of how hash table works. Collisions are not handled in this code.
+Please check https://github.com/Pravesh22/Python-_DSA/blob/master/hash_map/indermediate_hash_table.py"""
+
 
 class HashTable:
     def __init__(self, size=15):
@@ -41,11 +44,21 @@ class HashTable:
         h = self.get_hash(item)
         return self.array[h]
 
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.array[h] = None
+        return self.array
+
 
 if __name__ == '__main__':
     hash_table = HashTable()
     hash_table["ram"] = 24
-    print(hash_table.array)
+    hash_table["shyam"] = 25
+    hash_table["hari"] = 30
+    print("Original value in hash table", hash_table.array)
+    del hash_table["ram"]
+    print("After deleting ram value from hash table", hash_table.array)
+
     # print(hash_table.array)
     # hash_table.add("ram", 24)
     # hash_table.add("shyam", 25)
