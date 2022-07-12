c738231cd1cf816e7b7c98532a7e356390104b58
Pravesh22 <budhathokipravesh5@gmail.com>
2022-07-12 23:24:04 +0545

    search operation added in binary tree

--- tree/binary_tree.py
+++ tree/binary_tree.py
@@ -29,6 +29,20 @@ class BinaryTree:
             else:
                 self.right = BinaryTree(data)
 
+    def search(self, val):
+        if self.data == val:
+            return True
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
     def in_order_traversal(self):
         elements = []
 
@@ -54,4 +68,5 @@ def build_tree(elements):
 
 if __name__ == '__main__':
     numbers = [20, 88, 23, 15, 7, 14, 27, 12]
-    print(build_tree(numbers).in_order_traversal())
+    binary_tree = build_tree(numbers)
+    print(binary_tree.search(88))
